1d46f534b13affd5d58bd94c8d3bf30cbf9c9761
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-01-14 13:10:17 -0500

    Update shadcn/ui config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,12 @@
 import type { Config } from 'tailwindcss'
 
-export default {
+const config = {
   darkMode: ['class'],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
   prefix: '',
   theme: {
@@ -75,3 +76,5 @@ export default {
   },
   plugins: [require('tailwindcss-animate')],
 } satisfies Config
+
+export default config
