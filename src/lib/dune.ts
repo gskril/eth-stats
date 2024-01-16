74620f0d8356a847845ef756cf07ba26fe2b1242
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-01-16 13:28:42 -0500

    Get Dune data directly from queries

--- src/lib/dune.ts
+++ src/lib/dune.ts
@@ -11,18 +11,12 @@ const dune = new Dune(process.env.DUNE_API_KEY)
 
 export async function getDailyEthereumParticipants() {
   // https://dune.com/queries/41840
-  // const queryId = 41840
-  const executionId = '01HM5XQMWMJMP994WSM01KN3H8'
-  const data = await dune.results<DailyEthereumParticipants>(executionId)
-  return data
+  return await dune.results<DailyEthereumParticipants>(41840)
 }
 
 export async function getEthFinancialMetrics() {
   // https://dune.com/queries/2352666
-  // const queryId = 2352666
-  const executionId = '01HM51ZDDD7CGPBC3CJ9C5CR1N'
-  const ethFinancialMetrics =
-    await dune.results<EthFinancialMetrics>(executionId)
+  const ethFinancialMetrics = await dune.results<EthFinancialMetrics>(2352666)
   const rows = ethFinancialMetrics.result?.rows
 
   function ethFinancialMetric(metric: EthFinancialMetrics['metrics']) {
@@ -36,9 +30,8 @@ export async function getEthFinancialMetrics() {
 
 export async function getEthSupplyAndStakingMetrics() {
   // https://dune.com/queries/2418750
-  // const queryId = 2418750
-  const executionId = '01HM5HPHJNMEE7SZENKF00EY43'
-  const data = await dune.results<EthSupplyAndStakingMetrics>(executionId)
+  const data = await dune.results<EthSupplyAndStakingMetrics>(2418750)
+
   return {
     ethSupplyAndStakingMetrics: data.result?.rows[0],
     ethSupplyAndStakingMetricsExec: data.execution_ended_at,
@@ -47,8 +40,5 @@ export async function getEthSupplyAndStakingMetrics() {
 
 export async function getL2Summary() {
   // https://dune.com/queries/2858858
-  // const queryId = 2858858
-  const executionId = '01HM5SX8Q76PM6PVRQ8AHTDPTG'
-  const data = await dune.results<L2Summary>(executionId)
-  return data
+  return await dune.results<L2Summary>(2858858)
 }
